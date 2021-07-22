0c346b60bb6d2d51a27869b08c24d34e9818f459
Hoseong Lee <hoseong.a.lee@gmail.com>
2021-07-21 21:41:41 -0700

    Completed a script to convert CSV-formatted training data to JPEG images

--- scripts/convert-mouse-dynamics-csv-data-to-jpeg-images.js
+++ scripts/convert-mouse-dynamics-csv-data-to-jpeg-images.js
@@ -1,90 +1,13 @@
-const _ = require('lodash');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const { createCanvas } = require('canvas');
-
-const mouseOperations = [
-    {
-        state: "move",
-        x: 10,
-        y: 10,
-    },
-    {
-        state: "move",
-        x: 20,
-        y: 40,
-    },
-    {
-        state: "move",
-        x: 40,
-        y: 234,
-    },
-    {
-        state: "move",
-        x: 430,
-        y: 337,
-    },
-    {
-        state: "pressed",
-        x: 430,
-        y: 337
-    },
-    {
-        state: "drag",
-        x: 200,
-        y: 327
-    },
-    {
-        state: "drag",
-        x: 250,
-        y: 257
-    },
-    {
-        state: "released",
-        x: 430,
-        y: 200
-    },
-    {
-        state: "down",
-        x: 200,
-        y: 200,
-    },
-    {
-        state: "up",
-        x: 200,
-        y: 250,
-    },
-    {
-        state: "move",
-        x: 100,
-        y: 100,
-    },
-    {
-        state: "move",
-        x: 100,
-        y: 100,
-    },
-    {
-        state: "move",
-        x: 100,
-        y: 100,
-    },
-    {
-        state: "move",
-        x: 250,
-        y: 100,
-    },
-    {
-        state: "move",
-        x: 250,
-        y: 100,
-    },
-    {
-        state: "pressed",
-        x: 250,
-        y: 100
-    },
-];
+const csvParser = require('csv-parser');
+const { EventEmitter } = require('events');
+const {
+    NUMBER_OF_OPERATIONS,
+    TRAINING_DATA_USER_SESSION_FILES,
+    JPEG_TRAINING_DATA_DEST_DIR,
+} = require('../config/environment-variables');
 
 const handleMove = (ctx, { x, y }, { x: prevX, y: prevY }) => {
     if (prevX === null || prevY === null) {
@@ -93,7 +16,7 @@ const handleMove = (ctx, { x, y }, { x: prevX, y: prevY }) => {
     ctx.beginPath();
     ctx.moveTo(prevX, prevY);
     ctx.lineTo(x, y);
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 5;
     ctx.strokeStyle = '#ff0000';
     ctx.stroke();
 };
@@ -103,13 +26,13 @@ const handleClickAndDrag = (ctx, { state, x, y }, { state: prevState, x: prevX, 
         ctx.beginPath();
         ctx.moveTo(prevX, prevY);
         ctx.lineTo(x, y);
-        ctx.lineWidth = 4;
+        ctx.lineWidth = 10;
         ctx.strokeStyle = 'yellow';
         ctx.stroke();
     }
     if (state === 'pressed' && prevState !== 'pressed') {
         ctx.beginPath();
-        ctx.arc(x, y, 10, 0, 2 * Math.PI, false);
+        ctx.arc(x, y, 15, 0, 2 * Math.PI, false);
         ctx.fillStyle = 'blue';
         ctx.fill();
         ctx.strokeStyle = 'blue';
@@ -117,29 +40,32 @@ const handleClickAndDrag = (ctx, { state, x, y }, { state: prevState, x: prevX, 
     }
     if (state === 'released') {
         ctx.beginPath();
-        ctx.moveTo(x - 10, y - 10);
-        ctx.lineTo(x + 10, y + 10);
-        ctx.moveTo(x + 10, y - 10);
-        ctx.lineTo(x - 10, y + 10);
+        ctx.moveTo(x - 15, y - 15);
+        ctx.lineTo(x + 15, y + 15);
+        ctx.moveTo(x + 15, y - 15);
+        ctx.lineTo(x - 15, y + 15);
         ctx.strokeStyle = 'green';
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 7;
         return ctx.stroke();
     }
 };
 
-const handleScroll = (ctx, { state, x, y }) => {
-    ctx.beginPath();
-    ctx.moveTo(x - 10, y);
-    ctx.lineTo(x + 10, y);
-    if (state === 'up') {
-        ctx.lineTo(x, y - 20);
-    } else {
-        ctx.lineTo(x, y + 20);
+const handleScroll = (ctx, { state, x, y }, { state: prevState, x: prevX, y: prevY }) => {
+    if (prevX === 0 && prevY === 0) {
+       return;
     }
-    ctx.lineTo(x - 10, y);
+    ctx.beginPath();
+    ctx.moveTo(prevX - 20, prevY);
+    ctx.lineTo(prevX + 20, prevY);
+    if (state === 'up') {
+        ctx.lineTo(prevX, prevY - 35);
+    } else {
+        ctx.lineTo(prevX, prevY + 35);
+    }
+    ctx.lineTo(prevX - 20, prevY);
     ctx.fillStyle = '#32a88b';
     ctx.strokeStyle = '#0b261f';
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 4;
     ctx.fill();
     ctx.stroke();
 };
@@ -151,7 +77,7 @@ const handleStay = (ctx, { x: prevX, y: prevY, stayTime }) => {
 };
 
 const isStay = ({ state, x, y}, { state: prevState, x: prevX, y: prevY }) =>
-    state === prevState && x === prevX && y === prevY;
+    x === prevX && y === prevY;
 
 const getMaxSizeAndHeight = (mouseOperations =[]) => {
     let xMax = 0;
@@ -164,48 +90,79 @@ const getMaxSizeAndHeight = (mouseOperations =[]) => {
 };
 
 const createJpegImageFromMouseOperations = async (mouseOperations = [], destPath) => {
-    const { xMax, yMax } = getMaxSizeAndHeight(mouseOperations);
-    const canvas = createCanvas(xMax + 20, yMax + 20);
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    let prev = {
-        state: null,
-        x: null,
-        y: null,
-        stayTime: 0,
-    };
-    for (const current of mouseOperations) {
-        const { state, x, y } = current;
-        if (state === 'move') {
-            handleMove(ctx, current, prev);
-        } else if (state === 'pressed' || state === 'released' || state === 'drag') {
-            handleClickAndDrag(ctx, current, prev);
-        } else if (state === 'up' || state === 'down') {
-            handleScroll(ctx, current);
+    try {
+        const { xMax, yMax } = getMaxSizeAndHeight(mouseOperations);
+        const canvas = createCanvas(xMax + 20, yMax + 20);
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        let prev = {
+            state: null,
+            x: null,
+            y: null,
+            stayTime: 0,
+        };
+        for (const current of mouseOperations) {
+            current.state = current.state.toLowerCase();
+            current.x = parseInt(current.x);
+            current.y = parseInt(current.y);
+            const { state, x, y } = current;
+            if (state === 'move') {
+                handleMove(ctx, current, prev);
+            } else if (state === 'pressed' || state === 'released' || state === 'drag') {
+                handleClickAndDrag(ctx, current, prev);
+            } else if (state === 'up' || state === 'down') {
+                handleScroll(ctx, current, prev);
+            }
+            if (isStay(current, prev)) {
+                prev.stayTime += 1;
+            } else if (prev.stayTime > 0 && (!isStay(current, prev))) {
+                handleStay(ctx, prev);
+                prev.stayTime = 0;
+            }
+            prev.state = state;
+            prev.x = x;
+            prev.y = y;
         }
-        if (isStay(current, prev)) {
-            prev.stayTime += 1;
-        } else if (prev.stayTime > 0 && (!isStay(current, prev))) {
-            handleStay(ctx, prev);
-            prev.stayTime = 0;
-        }
-        prev.state = state;
-        prev.x = x;
-        prev.y = y;
+        const jpegImageBuffer = canvas.toBuffer('image/jpeg');
+        fs.writeFileSync(destPath, jpegImageBuffer);
+    } catch (err) {
+        console.error(err.message, destPath);
     }
-    const jpegImageBuffer = canvas.toBuffer('image/jpeg');
-    fs.writeFileSync(destPath, jpegImageBuffer);
 };
 
 const main = async () => {
-    const destDir = path.join(__dirname, '../dist/data/jpeg');
-    const numberOfMouseOperations = process.env.OPERATIONS || 1000;
-    const chunkedMouseOperations = _.chunk(mouseOperations, numberOfMouseOperations);
-    fs.mkdirSync(destDir, { recursive: true });
-    for (let i = 0; i < chunkedMouseOperations.length; i++) {
-       const mouseOperations = chunkedMouseOperations[i];
-       await createJpegImageFromMouseOperations(mouseOperations, path.join(destDir, `image-${i}.jpeg`));
+    const eventEmitter = new EventEmitter();
+
+    eventEmitter.on('create-image', async ({ number, user, session, mouseOperations }) => {
+        const imageFileName = `image-${number}.jpeg`;
+        const destDir = path.join(JPEG_TRAINING_DATA_DEST_DIR, user, session);
+        fs.mkdirSync(destDir, { recursive: true });
+        const imgPath = path.join(destDir, imageFileName);
+        await createJpegImageFromMouseOperations(mouseOperations, imgPath);
+    });
+
+    for (const { user, session, pathToFile } of TRAINING_DATA_USER_SESSION_FILES) {
+        let chunkedMouseOperations = [];
+        let number = 0;
+
+        const handleCsvDataStream = async data => {
+            if (data && chunkedMouseOperations.length < NUMBER_OF_OPERATIONS) {
+                return chunkedMouseOperations.push(data);
+            }
+            eventEmitter.emit('create-image', {
+                number,
+                user,
+                session,
+                mouseOperations: chunkedMouseOperations,
+            });
+            number += 1;
+            chunkedMouseOperations = [];
+        };
+        fs.createReadStream(pathToFile)
+            .pipe(csvParser())
+            .on('data', handleCsvDataStream)
+            .on('end', handleCsvDataStream);
     }
 };
 
